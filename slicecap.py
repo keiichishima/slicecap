b07c65cfd163f00750333c5cdca3e29f5d4f1d0c
Keiichi SHIMA <keiichi@iijlab.net>
2017-03-23 16:40:13 +0900

    stop using self._fo

--- slicecap.py
+++ slicecap.py
@@ -139,7 +139,6 @@ class Slicecap(object):
         self._offsets = []
         self._sizes = []
 
-        self._fo = open(self._options.infile, 'rb')
         self._unpack_file_header()
 
     @property
@@ -163,12 +162,13 @@ class Slicecap(object):
         return self._sizes
        
     def _unpack_file_header(self):
-        self._file_header.unpack_header(self._fo.read(24))
-        pph = PcapPkthdr()
-        pph.unpack_header(self._fo.read(16),
-                          self._file_header.byte_order)
-        self._base_tv_sec = pph.tv_sec
-        self._base_tv_usec = pph.tv_usec
+        with open(self._options.infile, 'rb') as _pfo:
+            self._file_header.unpack_header(_pfo.read(24))
+            pph = PcapPkthdr()
+            pph.unpack_header(_pfo.read(16),
+                              self._file_header.byte_order)
+            self._base_tv_sec = pph.tv_sec
+            self._base_tv_usec = pph.tv_usec
 
     def guess_frag_offsets_and_sizes(self):
         '''Try to slice the source pcap file into N files specified by the
@@ -177,26 +177,27 @@ class Slicecap(object):
         arrays.
 
         '''
-        for _frag_id in range(self._options.nslice):
-            _off = self._guess_offset_of_frag_id(_frag_id)
-            self._offsets.append(_off)
-            if _frag_id > 0:
-                self._sizes.append(_off - self._offsets[_frag_id - 1])
-        self._sizes.append(self._size - self._offsets[-1])
+        with open(self._options.infile, 'rb') as _pfo:
+            for _frag_id in range(self._options.nslice):
+                _off = self._guess_offset_of_frag_id(_pfo, _frag_id)
+                self._offsets.append(_off)
+                if _frag_id > 0:
+                    self._sizes.append(_off - self._offsets[_frag_id - 1])
+            self._sizes.append(self._size - self._offsets[-1])
 
-    def _guess_offset_of_frag_id(self, frag_id):
+    def _guess_offset_of_frag_id(self, pfo, frag_id):
         # Seek the file pointer to guessed position.
         _pcap_off_guess = frag_id * self._frag_size
-        self._fo.seek(_pcap_off_guess, 0)
+        pfo.seek(_pcap_off_guess, 0)
 
         # From the guessed position, read (snaplen + LLHDR len +
         # pcap_pkthdr len + margin) size data.  It must be enough long
         # to include the next pcap pkthdr data.
-        data = self._fo.read(self._file_header.snaplen + 1000)
+        data = pfo.read(self._file_header.snaplen + 1000)
 
         # Check if the file position is likely to match the pcap
         # pkthdr format by shifting the file pointer by 1 byte.
-        for _off_diff in range(len(data) - 4):
+        for _off_diff in range(len(data) - 16):
             pph = PcapPkthdr()
             pph.unpack_header(data[_off_diff:_off_diff + 16],
                               self._file_header.byte_order)
@@ -219,6 +220,7 @@ class Slicecap(object):
             #
             # XXX check frame contents for better validation
             return _pcap_off_guess + _off_diff
+        print('could not find pcap pkthdr at frag_id={}'.format(frag_id))
         raise ValueError
 
     def call_subcommands(self):
@@ -251,7 +253,10 @@ class Slicecap(object):
             _pfo.seek(_offset)
             _data_left = _size
             while _data_left > 0:
-                _data = _pfo.read(8192)
+                if _data_left > 8192:
+                    _data = _pfo.read(8192)
+                else:
+                    _data = _pfo.read(_data_left)
                 _proc.stdin.write(_data)
                 _data_left -= len(_data)
 
